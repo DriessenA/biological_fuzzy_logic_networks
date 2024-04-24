7e30240918eb169d329752a4bf96a1b12b33548d
Alice Driessen <alice@driessenmarks.nl>
2024-04-24 03:36:35 -0400

    Restored loss function and montecarlo_CV

--- Neurips_LMRL_workshop/thesis_results/CNoRFuzzy/montecarlo_CV.py
+++ Neurips_LMRL_workshop/thesis_results/CNoRFuzzy/montecarlo_CV.py
@@ -1,17 +1,26 @@
-import torch
-import argparse
-import os
+import warnings
 
+warnings.filterwarnings("ignore")
+
+import torch
+import networkx as nx
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import copy
+from tqdm import tqdm
+from datetime import datetime
+import pickle
 
-from sklearn.preprocessing import MinMaxScaler
+import biological_fuzzy_logic_networks.biofuzznet as biofuzznet
+import biological_fuzzy_logic_networks.biomixnet as biomixnet
+import biological_fuzzy_logic_networks.biofuzzdataset as biofuzzdataset
+import biological_fuzzy_logic_networks.Hill_function as Hill_function
+import biological_fuzzy_logic_networks.utils as utils
 
-from biological_fuzzy_logic_networks import biofuzznet
-from biological_fuzzy_logic_networks import biomixnet
-from biological_fuzzy_logic_networks import utils
-
+import argparse
+import os
 
 # Parse arguments
 parser = argparse.ArgumentParser()
@@ -48,12 +57,11 @@ else:
 
 # Load the data
 simulated_data = pd.read_csv(args.data, index_col=0)
-
 # Set the seed
 rng = np.random.default_rng(2)
 # Prepare the results dataframe
-RMSE_df = pd.DataFrame(index=simulated_data.columns)
-test_set_df = pd.DataFrame(index=["test_1", "test_2", "test_3", "test_4", "test_5"])
+RMSE_df = pd.DataFrame(columns=simulated_data.columns)
+test_set_df = pd.DataFrame(columns=["test_1", "test_2", "test_3", "test_4", "test_5"])
 if args.type == "fuzz":
     BFZ = biofuzznet.BioFuzzNet.build_BioFuzzNet_from_file(args.model)
 elif args.type == "mix":
@@ -61,7 +69,7 @@ elif args.type == "mix":
 else:
     ValueError(f"Unknown type {args.type}")
 BFZ_params = utils.obtain_params(BFZ)[0]
-params_df = pd.DataFrame(BFZ_params).T
+params_df = pd.DataFrame(BFZ_params)
 
 # X-folds cross validation
 for fold in range(args.foldnum):
@@ -76,39 +84,22 @@ for fold in range(args.foldnum):
     # Generate the data
     if len(simulated_data) == 22:  # Liver dataset
         input_col = ["IGF1", "IL1a", "IL6", "LPS", "TGFa", "TNFa"]
-        simulated_data = simulated_data[
-            input_col + [c for c in simulated_data if c not in input_col]
-        ]
         core_train = [1, 3, 4, 5, 6, 7, 8]
         train_rows = list(rng.choice([i for i in range(9, 23)], 9, replace=False))
         test_rows = [i for i in range(9, 23) if i not in train_rows and i not in [2]]
         train_rows = train_rows + core_train
-        train_data = simulated_data.loc[train_rows, :]
-        test_data = simulated_data.loc[test_rows, :]
-
-        scaler = MinMaxScaler()
-        train_data = pd.DataFrame(
-            scaler.fit_transform(train_data),
-            index=train_data.index,
-            columns=train_data.columns,
-        )
-        test_data = pd.DataFrame(
-            scaler.transform(test_data),
-            index=test_data.index,
-            columns=test_data.columns,
-        )
-        train_data[train_data > 1] = 1
-        test_data[test_data > 1] = 1
-
-        GT_train = {c: torch.tensor(train_data[c].values) for c in train_data.columns}
-        GT_test = {c: torch.tensor(test_data[c].values) for c in test_data.columns}
+        GT_train = {
+            c: torch.tensor(simulated_data.loc[train_rows, c].values)
+            for c in simulated_data.columns
+        }
+        GT_test = {
+            c: torch.tensor(simulated_data.loc[test_rows, c].values)
+            for c in simulated_data.columns
+        }
         input_train = {c: GT_train[c] for c in input_col}
         input_test = {c: GT_test[c] for c in input_col}
     elif len(simulated_data) == 16:  # DREAM dataset
         input_col = ["tgfa", "igf1", "il1a", "tnfa"]
-        simulated_data = simulated_data[
-            input_col + [c for c in simulated_data if c not in input_col]
-        ]
         core_train = [1, 2, 3, 4, 5]
         train_rows = list(rng.choice([i for i in range(6, 16)], 5, replace=False))
         test_rows = [i for i in range(6, 16) if i not in train_rows]
@@ -151,6 +142,16 @@ for fold in range(args.foldnum):
                 f"{args.outputfolder}/fold_{fold}_intermediate_loss_{i}.png"
             )
 
+        # Concatenate the losses of the iterative runs
+        # all_losses = pd.concat(losses)
+        # all_losses.reset_index(inplace=True)
+        # all_losses
+
+        # Plot the losses to check optimisation happened correctly
+        # plot_loss = sns.lineplot(data=all_losses, x="time", y="loss", hue="phase")
+        # plot_fig = plot_loss.get_figure()
+        # plot_fig.savefig(f"{args.outputfolder}/total_loss_CV{fold}.png")
+
         # Generate the measures that will be useful
         # Simulate the BFZ on the test set
         BFZ.set_network_ground_truth(input_test)
@@ -158,17 +159,17 @@ for fold in range(args.foldnum):
         # Compute the test error and save it into the dataframe
         output_RMSE = utils.compute_RMSE_outputs(model=BFZ, ground_truth=GT_test)
         RMSE = pd.Series(output_RMSE)
-        RMSE_df[fold] = RMSE
+        RMSE_df = RMSE_df.append(RMSE, ignore_index=True)
 
         # Save the transfer edges parameters
         BFZ_params = utils.obtain_params(BFZ)[0]
         params = pd.Series(BFZ_params)
-        params_df[fold] = params
+        params_df = params_df.append(params, ignore_index=True)
 
         # Save the input combinations that we used for testing
         input_combi = pd.Series(test_rows)
         input_combi.index = ["test_1", "test_2", "test_3", "test_4", "test_5"]
-        test_set_df[fold] = input_combi
+        test_set_df = test_set_df.append(input_combi, ignore_index=True)
 
     except Exception as e:
         print(f"Fold {fold} was interrupted because of error {e}")
