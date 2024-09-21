b08a83170a97841eaf92060aa1c80cdccce8ec8c
Alice Driessen <adr@zurich.ibm.com>
2024-09-21 17:23:01 +0200

    merge

--- biological_fuzzy_logic_networks/manuscripts_functions/metrics.py
+++ biological_fuzzy_logic_networks/manuscripts_functions/metrics.py
@@ -9,78 +9,79 @@ import pandas as pd
 from typing import Iterable
 
 
-def maximum_mean_discrepancy(target, transport, gamma: float) -> float:
+def maximum_mean_discrepancy(true, pred, gamma: float) -> float:
     """Calculates the maximum mean discrepancy between two measures."""
-    target, transport = prepare_arrays([target, transport])
-    xx = rbf_kernel(target, target, gamma)
-    xy = rbf_kernel(target, transport, gamma)
-    yy = rbf_kernel(transport, transport, gamma)
+    true, pred = prepare_arrays([true, pred])
+    xx = rbf_kernel(true, true, gamma)
+    xy = rbf_kernel(true, pred, gamma)
+    yy = rbf_kernel(pred, pred, gamma)
 
     return float(xx.mean() + yy.mean() - 2 * xy.mean())
 
 
 def compute_scalar_mmd(
-    target: jnp.ndarray,
-    transport: jnp.ndarray,
+    true: np.ndarray,
+    pred: np.ndarray,
     gammas: list[float] = [2, 1, 0.5, 0.1, 0.01, 0.005],
 ):
     """
-    Calculates the maximum mean discrepancy between the target
-    and the transported measures, using gaussian kernel,averaging for different gammas.
+    Calculates the maximum mean discrepancy between the true
+    and the preded measures, using gaussian kernel,averaging for different gammas.
     """
-    target, transport = prepare_arrays([target, transport])
+    true, pred = prepare_arrays([true, pred])
 
     def safe_mmd(*args):
         try:
             mmd = maximum_mean_discrepancy(*args)
         except ValueError:
-            mmd = jnp.nan
+            mmd = np.nan
         return mmd
 
-    return float(np.mean(list(map(lambda x: safe_mmd(target, transport, x), gammas))))
+    return float(np.mean(list(map(lambda x: safe_mmd(true, pred, x), gammas))))
 
 
-def average_r2(target: jnp.ndarray, transport: jnp.ndarray) -> float:
+def average_r2(true: np.ndarray, pred: np.ndarray) -> float:
     """
-    Calculate the correlation coefficient r^2 between the means of average features in target and tansport.
+    Calculate the correlation coefficient r^2 between the means of average features in true and tansport.
     """
-    target, transport = prepare_arrays([target, transport])
-    target_means = jnp.mean(target, axis=0)
-    transport_means = jnp.mean(transport, axis=0)
-    average_r2 = np.corrcoef(target_means, transport_means)[0, 1] ** 2
+    true, pred = prepare_arrays([true, pred])
+    true_means = np.mean(true, axis=0)
+    pred_means = np.mean(pred, axis=0)
+    average_r2 = np.corrcoef(true_means, pred_means)[0, 1] ** 2
     return float(average_r2)
 
 
 def wasserstein_distance(
-    target: jnp.ndarray, transport: jnp.ndarray, epsilon: float = 0.1
+    true: np.ndarray, pred: np.ndarray, epsilon: float = 0.1
 ) -> float:
     """
     Calculates the Wasserstain distance between two measures
     using the Sinkhorn algorithm on the regularized OT formulation.
     """
-    target, transport = prepare_arrays([target, transport])
-    geom = PointCloud(target, transport, cost_fn=costs.Euclidean(), epsilon=epsilon)
+    true, pred = prepare_arrays([true, pred], jax=True)
+    geom = PointCloud(true, pred, cost_fn=costs.Euclidean(), epsilon=epsilon)
     solver = jax.jit(sinkhorn.solve)
     ot = solver(geom)
     return ot.reg_ot_cost
 
 
-def prepare_arrays(dfs: Iterable):
+def prepare_arrays(dfs: Iterable, jax:bool=False):
     # Select only numeric columns
     return_dfs = []
     for df in dfs:
         if isinstance(df, pd.DataFrame):
             df = df.select_dtypes(include="number")
-            jax_array = jnp.array(df)
-            return_dfs.append(jax_array)
+            array = np.array(df)
+            return_dfs.append(array)
         elif isinstance(df, np.ndarray):
-            jax_array = jnp.array(df)
-            return_dfs.append(jax_array)
-        elif isinstance(df, jnp.ndarray):
+            array = np.array(df)
+            return_dfs.append(array)
+        elif isinstance(df, np.ndarray):
             return_dfs.append(df)
         else:
             Exception(
                 "Class not recognized, please pass pandas dataframe or a (jax) numpy array"
             )
-
+    if jax:
+        return_dfs = [jnp.array(df) for df in return_dfs]
     return return_dfs
